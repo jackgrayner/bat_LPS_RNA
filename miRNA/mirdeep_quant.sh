4fc69ad7d1cdd8bd3071fc5296ac2abdb556bfd4
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2025-08-08 12:19:25 -0400

    Update mirdeep_quant.sh

--- miRNA/mirdeep_quant.sh
+++ miRNA/mirdeep_quant.sh
@@ -12,3 +12,12 @@ while read sample
 do
 	/mnt/apps/users/jrayner/conda/envs/mirdeep2/bin/quantifier.pl -y ${sample} -d -p ../phha_mammal_hairpin.fa -m ../phha_mammal_mature.fa -r ../${sample}_collapsed.fa 
 done < ../../sample_list
+
+while read sample
+do
+	cat miRNAs_expressed_all_samples_${sample}.csv | awk '{print $2}' > ${sample}_expression
+done < ../../sample_list
+
+cat miRNAs_expressed_all_samples_2311_TH.csv | awk '{print $3}' > mirna_ids
+paste mirna_ids *_expression > all_samples_expression.tsv
+
