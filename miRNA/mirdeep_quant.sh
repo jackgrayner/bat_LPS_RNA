6149530a282dea490ee2a6c84f9ac40071ccc3dc
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2025-09-12 15:15:13 -0400

    Update mirdeep_quant.sh

--- miRNA/mirdeep_quant.sh
+++ miRNA/mirdeep_quant.sh
@@ -8,9 +8,16 @@
 #SBATCH --mail-user=jrayner1@umd.edu
 #SBATCH --output=mirdeep_quant.log
 source activate mirdeep2
+
+#remove duplicate mature sequences (sorted by seq, then sorted by name, then dupes removed - in R)
+while read line
+do
+  grep -A 1 -w $line phha_mammal_mature.fa >> phha_mammal_mature_unique.fa
+done < keep_dedup.txt
+
 while read sample
 do
-	/mnt/apps/users/jrayner/conda/envs/mirdeep2/bin/quantifier.pl -y ${sample} -d -p ../phha_mammal_hairpin.fa -m ../phha_mammal_mature.fa -r ../${sample}_collapsed.fa 
+	/mnt/apps/users/jrayner/conda/envs/mirdeep2/bin/quantifier.pl -y ${sample} -d -p ../phha_mammal_hairpin.fa -m ../phha_mammal_mature_unique.fa -r ../${sample}_collapsed.fa 
 done < ../../sample_list
 
 while read sample
