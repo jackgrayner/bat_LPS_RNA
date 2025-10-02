6ae4fe37371c4319f5af141ecbfbe5bd10fd9de5
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2025-10-02 17:22:31 -0400

    Update 04.filter_novel_miRNAs.R

--- miRNA/04.filter_novel_miRNAs.R
+++ miRNA/04.filter_novel_miRNAs.R
@@ -1,13 +1,13 @@
-nov<-read.csv("~/Documents/UMD_new/nih_rna/miRNA/novel_pha_rnas.csv",h=T)
+nov<-read.csv("result_02_10_2025_t_20_06_39.tsv",h=T,sep="\t")
 head(nov)
 nrow(nov)
 
 #get rid of low score, rfam alerts, lowly expressed miRNAs, and miRNAs with same seed as others
-nov<-nov[nov$miRDeep2.score>10 & nov$rfam.alert=="-" & nov$total.read.count>10 & nov$example.miRBase.miRNA.with.the.same.seed=="-",]
+nov<-nov[nov$miRDeep2.score>10 & nov$rfam.alert=="-" & nov$total.read.count>10 & nov$example.miRBase.miRNA.with.the.same.seed=="-" & nov$significant.randfold.p.value=="yes",]
 #assign random IDs
-nov$Ph_ID<-paste0("phha-miR-",c(1:nrow(nov)))
+nov$Ph_ID<-paste0("phha-miR-",c(1000:(999+nrow(nov))))
 
 nov$consensus.mature.sequence<-toupper(nov$consensus.mature.sequence)
 nov$consensus.precursor.sequence<-toupper(nov$consensus.precursor.sequence)
-write.csv(nov[,c("Ph_ID","consensus.precursor.sequence")],file="~/Documents/UMD_new/nih_rna/miRNA/novel_pha_rnas_filtered_precursor.csv",row.names=FALSE,quote=FALSE)
-write.csv(nov[,c("Ph_ID","consensus.mature.sequence")],file="~/Documents/UMD_new/nih_rna/miRNA/novel_pha_rnas_filtered_mature.csv",row.names=FALSE,quote=FALSE)
+write.csv(nov[,c("Ph_ID","consensus.precursor.sequence")],file="novel_pha_rnas_filtered_precursor.csv",row.names=FALSE,quote=FALSE)
+write.csv(nov[,c("Ph_ID","consensus.mature.sequence")],file="novel_pha_rnas_filtered_mature.csv",row.names=FALSE,quote=FALSE)
