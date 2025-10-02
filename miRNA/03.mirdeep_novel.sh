5bdba7c702c3065024dead19aa86667ac2c057b2
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2025-10-02 15:17:18 -0400

    Update 03.mirdeep_novel.sh

--- miRNA/03.mirdeep_novel.sh
+++ miRNA/03.mirdeep_novel.sh
@@ -8,8 +8,15 @@
 #SBATCH --mail-user=jrayner1@umd.edu
 #SBATCH --output=mirdeep_novel.log
 source activate mirdeep2
-cat ../*collapsed.fa > all_collapsed.fa
-#nb bat sequences from Li et al. 2022, Ecol & Evol
-#mammal seqs from mirbase
+#concatenate
+cat ../*collapsed.fa > all_collapsed1.fa
+#collapse concatenated fasta
 /mnt/apps/users/jrayner/conda/envs/mirdeep2/bin/mapper.pl all_collapsed1.fa -c -o 8 -i -j -m -s all_collapsed.fa -t all_collapsed.arf -p ../phha
-/mnt/apps/users/jrayner/conda/envs/mirdeep2/bin/miRDeep2.pl all_collapsed.fa ../phha_genomic_nowhitespace.fa all_collapsed.arf none ../bats_mammal_mature_nowhitespace.fa ../mammal_hairpin_nowhitespace.fa 
+
+#keep only human, mouse, bat miRNAs
+grep -A 1 "^>hsa\|^>mmu\|^>aja\|^>pal\|^>efu" hairpin_nowhitespace.fa | grep -v "\-\-" > mirbase_bats_mammal_hairpin.fa
+grep -A 1 "^>hsa\|^>mmu\|^>aja\|^>pal\|^>efu" mature_nowhitespace.fa | grep -v "\-\-" > mirbase_bats_mammal_mature.fa
+cat mirbase_bats_mammal_hairpin.fa mlu_precursor.fa mmy_precursor.fa > all_hairpin.fa
+
+#now identify novel transcripts
+/mnt/apps/users/jrayner/conda/envs/mirdeep2/bin/miRDeep2.pl all_collapsed.fa ../phha_genomic_nowhitespace.fa all_collapsed.arf none mirbase_bats_mammal_mature.fa all_hairpin.fa
